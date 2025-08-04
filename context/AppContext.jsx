37f66a4c9e35d78ac0c64fbe0656b82a86caaeb9
dionfarado <dionfarado192@gmail.com>
2025-08-04 13:51:06 +0700

    First Commit

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,5 +1,6 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
+import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
 
@@ -10,6 +11,7 @@ export const useAppContext = () => {
 }
 
 export const AppContextProvider = (props) => {
+    const {user}=useUser()
 
     const currency = process.env.NEXT_PUBLIC_CURRENCY
     const router = useRouter()
@@ -82,6 +84,7 @@ export const AppContextProvider = (props) => {
     }, [])
 
     const value = {
+        user,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
